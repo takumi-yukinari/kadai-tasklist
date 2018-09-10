434303e4724d1d7178092686ae6f7d7fb79c0740
takumi.yukinari <takumi.yukinari@axalpha.com>
2018-09-10 06:20:33 +0000

    add column

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスク新規作成ページ</h1>
+    
+    @if (count($errors)>0)
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error}}</li>
+            @endforeach
+        </ul>
+    @endif
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        {!! Form::label('status', 'status :') !!}
+        {!! Form::text('status') !!}
+
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+@endsection
